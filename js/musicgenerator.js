cc53c253131f583307c3ac5d19d3426799a16464
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2013-04-01 01:02:57 +0300

    First commit.

---
+++ js/musicgenerator.js
@@ -0,0 +1,32 @@
+var MusicGenerator = (function(options) {
+
+    var defaultOptions = {
+
+    };
+    options = $.extend(true, {}, defaultOptions, options);
+
+    var my = {};
+
+    function init() {
+
+    }
+
+    //
+    // Public methods
+    //
+
+    my.compose = function() {
+
+    };
+
+    //
+    // Private methods
+    //
+
+    function a() {
+
+    }
+
+    init();
+    return my;
+});
