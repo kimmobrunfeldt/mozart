eb8f0f2ff7c0df958ebac1d7e17536a6967a8fb0
Vesa Uimonen <vesa.uimonen@gmail.com>
2013-04-01 15:24:52 +0300

    Added playback display fadeIn

--- js/player.js
+++ js/player.js
@@ -9,7 +9,11 @@ define([
       chords.forEach(function(chord) {
         MIDI.chordOn(0, chord.notes, chord.velocity, chord.delay);
         setTimeout(function() {
-          $('#playback').append(chord.str);
+          var $letter = $('<span></span>');
+          $('#playback').append($letter);
+          $letter.hide();
+          $letter.append(chord.str);
+          $letter.fadeIn('fast');
         }, chord.delay * 1000);
       });
     });
